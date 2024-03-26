59990e00886a5fb1c317ca2dea6ea142fcc212c8
thiagofern4ndo <thiagofern4ndo@gmail.com>
2024-03-26 20:49:04 -0300

    envia primeiro commit de teste

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Primeiro commit de exemplo'),
     );
   }
 }
