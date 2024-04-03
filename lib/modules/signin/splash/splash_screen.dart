a906730247f2a01c81259cf3881a8739cb601d6a
Thiago Fernando <153394874+thiagofern4ndo@users.noreply.github.com>
2024-04-03 06:54:49 -0300

    splash screen com as alteraçoes solicitadas e mudança de diretorio

---
+++ lib/modules/signin/splash/splash_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:economiza_facil/shared/assets/image_strings.dart';
+import 'package:economiza_facil/modules/signin/login/login_page.dart'; 
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _loadNextScreen(context); 
+    
+  }
+
+  void _loadNextScreen(BuildContext context) async {
+  
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        if (mounted) {
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (BuildContext context) => const LoginPage(), // Redireciona para a página de login
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 132, 36, 1), // Cor de fundo
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [ 
+            SvgPicture.asset(
+              ImageStrings.logoSplash,
+              width: 180,
+              height: 180,
+            ),
+            const Text(
+              'Economiza\nFácil',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 45,
+                color: Colors.white,
+                fontWeight: FontWeight.bold, 
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
