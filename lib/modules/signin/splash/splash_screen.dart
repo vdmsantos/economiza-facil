5a991cf0c7cb46313fd664f4fc19c3e71e792af0
Vitor Daniel Mendes dos Santos <94265037+mendesviton@users.noreply.github.com>
2024-04-08 08:51:17 -0300

    feat: add more features

--- lib/modules/signin/splash/splash_screen.dart
+++ lib/modules/signin/splash/splash_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:economiza_facil/shared/assets/image_strings.dart';
-import 'package:economiza_facil/modules/signin/login/login_page.dart'; 
+import 'package:economiza_facil/modules/signin/login/login_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
-static String route = '/';
+  static String route = '/';
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -14,34 +14,33 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _loadNextScreen(context); 
-    
+    _loadNextScreen(context);
   }
 
- void _loadNextScreen(BuildContext context) async {
-  
+  void _loadNextScreen(BuildContext context) async {
     Future.delayed(
       const Duration(seconds: 3),
       () {
         if (mounted) {
           Navigator.of(context).pushReplacement(
             MaterialPageRoute(
-              builder: (BuildContext context) => const LoginPage(), 
+              builder: (BuildContext context) => const LoginPage(),
             ),
           );
         }
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(244, 132, 36, 1), 
+      backgroundColor: const Color.fromRGBO(244, 132, 36, 1),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [ 
+          children: [
             SvgPicture.asset(
               ImageStrings.logoSplash,
               width: 180,
@@ -53,7 +52,7 @@ class _SplashScreenState extends State<SplashScreen> {
               style: TextStyle(
                 fontSize: 45,
                 color: Colors.white,
-                fontWeight: FontWeight.bold, 
+                fontWeight: FontWeight.bold,
               ),
             ),
           ],
@@ -62,5 +61,3 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-
-
--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,6 @@ import 'package:economiza_facil/modules/signin/splash/splash_screen.dart';
 import 'package:economiza_facil/modules/signin/login/login_page.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  SplashScreen.route: (context) => SplashScreen(),
-  LoginPage.route: (context) => LoginPage(), 
+  SplashScreen.route: (context) => const SplashScreen(),
+  LoginPage.route: (context) => const LoginPage(),
 };
